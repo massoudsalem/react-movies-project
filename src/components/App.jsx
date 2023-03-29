2bb0dcacad56b190602bf63c9dedfa37b5b6bd24
Mohammad Massoud <36207910+massoudsalem@users.noreply.github.com>
2023-03-29 19:03:15 +0200

    adding Components Structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { CssBaseline } from '@mui/material';
+import { Actors, MovieInformation, Movies, NavBar, Profile } from '.';
 
 const App = () => (
   <div>
+    <NavBar />
     <main>
       <CssBaseline />
       <Routes>
-        <Route path="/" element={<h1>Movies</h1>} />
-        <Route path="/movies/:id" element={<h1>Movies Information</h1>} />
-        <Route path="/actors/:id" element={<h1>Actors</h1>} />
-        <Route path="/profile/:id" element={<h1>Profile</h1>} />
+        <Route path="/" element={<Movies />} />
+        <Route path="/movies/:id" element={<MovieInformation />} />
+        <Route path="/actors/:id" element={<Actors />} />
+        <Route path="/profile/:id" element={<Profile />} />
       </Routes>
     </main>
   </div>
